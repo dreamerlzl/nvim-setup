809520256480991819254bb92a035fd97c4a43bf
dreamerlzl <875350879@qq.com>
2023-04-12 22:54:10 +0800

    feat: toggle sign, fold, line and scrollbar all at once

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,30 +10,48 @@ local function map(mode, lhs, rhs, opts)
 	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-vim.cmd([[
-nnoremap <C-s> :call ToggleSignColumn()<CR>
-" Toggle signcolumn. Works on vim>=8.1 or NeoVim
-function! ToggleSignColumn()
-    if !exists("b:signcolumn_on") || b:signcolumn_on
-        set signcolumn=no
-        let b:signcolumn_on=0
-    else
-        set signcolumn=number
-        let b:signcolumn_on=1
-    endif
-endfunction
-]])
+--vim.cmd([[
+--nnoremap <C-s> :call ToggleSignColumn()<CR>
+--" Toggle signcolumn. Works on vim>=8.1 or NeoVim
+--function! ToggleSignColumn()
+--    if !exists("b:signcolumn_on") || b:signcolumn_on
+--        set signcolumn=no
+--        let b:signcolumn_on=0
+--    else
+--        set signcolumn=number
+--        let b:signcolumn_on=1
+--    endif
+--endfunction
+--]])
+
+map("n", "<C-s>", ":call ToggleSignColumn()<CR>")
+function ToggleSignColumn()
+	if not vim.b.signcolumn_on or vim.b.signcolumn_on == 1 then
+		vim.opt.signcolumn = "no"
+		vim.b.signcolumn_on = 0
+	else
+		vim.opt.signcolumn = "number"
+		vim.b.signcolumn_on = 1
+	end
+end
 
 -- Toggle LineNumberColumn
-map("n", "<C-n>", ":set invnumber | exec ToggleAll() <CR>")
-
-vim.cmd([[
+map("n", "<C-n>", ":set invnumber | lua ToggleAll() <CR>")
 
 function ToggleAll()
-  :ScrollbarToggle
-endfunction
+	vim.cmd("ScrollbarToggle")
+	vim.cmd("IndentBlanklineToggle")
+	ToggleFoldColumn()
+	ToggleSignColumn()
+end
 
-]])
+function ToggleFoldColumn()
+	if vim.o.foldcolumn == 0 then
+		vim.o.foldcolumn = "1"
+	else
+		vim.o.foldcolumn = "0"
+	end
+end
 
 -- buffers
 -- for switching tabs
