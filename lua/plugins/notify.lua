1aa4ad168f8de8eded0e693e349cad262e90f59a
dreamerlzl <875350879@qq.com>
2023-04-27 20:16:16 +0800

    fix: noice for leaderF

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,33 +2,42 @@ vim.notify = require("notify")
 
 -- table from lsp severity to vim severity.
 local severity = {
-	"error",
-	"warn",
-	"info",
-	"info", -- map both hint and info to info?
+  "error",
+  "warn",
+  "info",
+  "info", -- map both hint and info to info?
 }
 vim.lsp.handlers["window/showMessage"] = function(err, method, params, client_id)
-	vim.notify(method.message, severity[params.type])
+  vim.notify(method.message, severity[params.type])
 end
 
 require("noice").setup({
-	lsp = {
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true,
-		},
-	},
-	-- you can enable a preset for easier configuration
-	presets = {
-		bottom_search = true, -- use a classic bottom cmdline for search
-		-- command_palette = true, -- position the cmdline and popupmenu together
-		long_message_to_split = true, -- long messages will be sent to a split
-		inc_rename = false, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = false, -- add a border to hover docs and signature help
-	},
-	messages = {
-		enabled = false,
-	},
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true,       -- use a classic bottom cmdline for search
+    -- command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false,         -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,     -- add a border to hover docs and signature help
+  },
+  -- for LeaderfFile + noice
+  routes = {
+    {
+      view = "notify",
+      filter = {
+        any = {
+          { kind = "echo" },
+        },
+      },
+      opts = { skip = true },
+    },
+  },
 })
