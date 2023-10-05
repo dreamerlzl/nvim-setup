38bb21764d473eac1851434f8784907868e2e9b4
dreamerlzl <875350879@qq.com>
2023-10-05 10:42:20 +0800

    fix: ibl

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -24,6 +24,4 @@ end)
 require("ibl").setup({
 	indent = { highlight = highlight },
 	-- for example, context is off by default, use this to turn it on
-	show_current_context = true,
-	show_current_context_start = true,
 })
