325fa687c023a277339ce593c4159797d6bf8e93
dreamerlzl <875350879@qq.com>
2023-10-17 13:48:50 +0800

    fix: lsp-progress

--- lua/core/statusline.lua
+++ lua/core/statusline.lua
@@ -58,7 +58,8 @@ require("lualine").setup({
 })
 
 vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-vim.api.nvim_create_autocmd("User LspProgressStatusUpdated", {
+vim.api.nvim_create_autocmd("User", {
 	group = "lualine_augroup",
+	pattern = "LspProgressStatusUpdated",
 	callback = require("lualine").refresh,
 })
