492b20e5dc3b669c258b79143afd2f5ee90ec3d3
dreamerlzl <875350879@qq.com>
2026-04-07 10:29:22 +0800

    refactor: replace vim-vsnip with luanip

--- lua/core/cmp.lua
+++ lua/core/cmp.lua
@@ -1,17 +1,36 @@
 local cmp = require("cmp")
+local lspkind = require("lspkind")
+local luasnip = require("luasnip")
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
 cmp.setup({
-	-- Enable LSP snippets
 	snippet = {
 		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
+			luasnip.lsp_expand(args.body)
 		end,
 	},
 	mapping = {
 		["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-n>"] = cmp.mapping.select_next_item(),
-		-- Add tab support
-		["<S-Tab>"] = cmp.mapping.select_prev_item(),
-		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-s>"] = cmp.mapping.complete(),
@@ -21,14 +40,12 @@ cmp.setup({
 			select = true,
 		}),
 	},
-
-	-- Installed sources
 	sources = {
 		{
 			name = "nvim_lsp",
 		},
 		{
-			name = "vsnip",
+			name = "luasnip",
 		},
 		{
 			name = "path",
@@ -37,10 +54,6 @@ cmp.setup({
 			name = "buffer",
 		},
 	},
-})
-
-local lspkind = require("lspkind")
-cmp.setup({
 	formatting = {
 		format = lspkind.cmp_format({
 			mode = "symbol_text", -- show only symbol annotations
--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -699,13 +699,11 @@ lazy.setup({
 			-- these dependencies will only be loaded when cmp loads
 			-- dependencies are always lazy-loaded unless specified otherwise
 			dependencies = {
-				"L3MON4D3/LuaSnip",
+				{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
 				"hrsh7th/cmp-nvim-lsp",
 				"hrsh7th/cmp-path",
 				"hrsh7th/cmp-buffer",
 				"saadparwaiz1/cmp_luasnip",
-				"hrsh7th/cmp-vsnip",
-				"hrsh7th/vim-vsnip",
 				"rafamadriz/friendly-snippets",
 			},
 			config = function()
