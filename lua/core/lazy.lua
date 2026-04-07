2d7e47320f3e4cc8e83ea99b40b33486f12d65b1
dreamerlzl <875350879@qq.com>
2026-04-07 10:36:54 +0800

    fix: ensure lua lsp

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -621,7 +621,7 @@ lazy.setup({
 			end,
 			config = function()
 				require("mason-lspconfig").setup({
-					ensure_installed = { "tinymist", "terraformls" },
+					ensure_installed = { "lua_ls", "tinymist", "terraformls" },
 					automatic_setup = false,
 					automatic_enable = false,
 				})
